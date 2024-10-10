e00cb5e48797796c5080dc74e363bae50a79fea2
Lybrook <omoshlybrook@gmail.com>
2024-10-10 21:59:21 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const driversList = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const driversWithDetails = [
+  { name: 'Bobby', hometown: 'Pittsburgh' },
+  { name: 'Sammy', hometown: 'New York' },
+  { name: 'Sally', hometown: 'Cleveland' },
+  { name: 'Annette', hometown: 'Los Angeles' },
+  { name: 'Bobby', hometown: 'Tampa Bay' }
+];
+
+const findMatching = (drivers, name) => drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (drivers, letters) => drivers.filter((driver) => driver.startsWith(letters));
+
+const matchName = (drivers, name) => drivers.filter((driver) => driver.name === name);
+
+  matchingDrivers: findMatching(driversList, 'Bobby'),
+  fuzzyDrivers; fuzzyMatch(driversList, 'Sa'),
+  matchedDriversWithName; matchName(driversWithDetails, 'Bobby')
+;
